e3b246cb3204408b03f75810c8876f45f951fa88
marcusbotelho <marcus.itec@gmail.com>
2018-02-03 22:43:30 -0300

    UPGRADE: Corrections in animations and JS code.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-window.addEventListener('keydown', function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    function playSound (e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    }
 
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-    setTimeout(function () {
-       key.classList.remove('playing');
-    }, 200)
-});
+    function removeTransition(e) {
+        if(e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+    }
+
+    const keys = document.querySelectorAll('.key');
+
+    keys.forEach(key => {
+        key.addEventListener('transitionend', removeTransition)
+    });
+
+    window.addEventListener('keydown', playSound);
